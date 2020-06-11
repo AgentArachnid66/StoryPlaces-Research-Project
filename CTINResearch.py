cdeafe446502bcd041bfeb5762610feebdb039c9
AgentArachnid66 <50571566+AgentArachnid66@users.noreply.github.com>
2020-06-11 17:54:35 +0100

    Finished EDA

--- CTINResearch.py
+++ CTINResearch.py
@@ -42,16 +42,18 @@ pagePerUser  = pd.DataFrame()
 
 # The first thing I did was remove any rows that don't have any page IDs,
 # Therefore they didn't read anything and shouldn't be included in the count
-validPages = dataSet.copy().dropna(how='any', subset=['pageId'])
+validPages = dataSet.copy(deep=True).dropna(how='any', subset=['pageId'])
 
 # After that, it was a simple case of grouping them by user and count the number
 # Of occurances and store that as it's own variable
 pagePerUser = validPages.groupby('user', as_index=False)['pageId'].count()
-# Print the first 20 users with the pages read for each user
+# Removes all columns except the relevant ones
 pagePerUser = pagePerUser[["user", "pageId"]]
+# Changes the pages Id column to pagesRead as it's now a count of how many
+# pages were read by a user.
 pagePerUser.rename(columns={"pageId" : "pagesRead"}, inplace=True)
 
-
+# Save this Data Frame as a csv
 pagePerUser.to_csv("PagePerUserDF.csv")
 
 print("User: " +pagePerUser['user'].astype(str) +"    Pages Read: " +pagePerUser['pagesRead'].astype(str))
@@ -94,7 +96,8 @@ storyFreqU = validStories[["user", "storyId"]].groupby(['storyId','user'])
 #%%
 # This cell is the Exploratory Data Analysis - Time Spent on each Page
 
-# Checks if any rows have no date information or page Id.
+# Checks if any rows have no date information or page Id and copies the 
+# data frame to the variable.
 validPageTimes = dataSet.copy().dropna(how='any', subset=['date', 'pageId'])
 
 # Generates a new DataFrame for the time spent on each page based on user, 
@@ -103,6 +106,10 @@ timePage = validPageTimes[['user', 'date', 'pageId']].groupby(['user', 'date','p
 
 # Converts the date series into datetime
 timePage['date'] = pd.to_datetime(timePage['date'])
+
+# Copy the Data frame to another variable for further manipulation in another
+# cell.
+userPage = timePage.copy(deep=True)
 
 # Goes through the group and calculates the difference between the dates and 
 # shifts it up one row so that it matches to the correct pageId
@@ -117,20 +124,42 @@ exitPoints = exitPoints.drop('timeOnPage', 1)
 
 # Drops NaN values from the data set
 timePage = timePage.dropna(how='any', subset = ['timeOnPage'])
-timePage.to_csv("TimeSpentOnPage")
+timePage.to_csv("TimeSpentOnPage.csv")
 
 print(organiseDescribe(timePage['timeOnPage'].describe()))
 
 
 #%%
 
-
 # This cell is the Exploratory Data Analysis - Exit Point Frequency
 # This is where each user stops reading. I can use the exitPoints dataframe
 # I made in a previous cell as a basis for this. I just need to group it by
 # page and count if there are any pages that are the most likely to be the 
 # exit point.
-print(exitPoints.groupby('pageId').ngroups)
 
-(exitPoints.groupby('pageId').size().sort_values(ascending = False)).to_csv("ExitPoints.csv")
+sortExit = (exitPoints.groupby('pageId').size().sort_values(ascending = False))
+print(sortExit.head())
+sortExit.to_csv("ExitPoints.csv")
 
+#%%
+
+#This cell is the Exploratory Data Analysis - Total Time Per User
+
+# Groups by user and gets the first and last date values in the group
+userTime = userPage.groupby('user')['date'].agg(['first','last'])
+# Renames the columns to more appropriate terms
+userTime.columns = ["login time","logout time"]
+# Filter to get rid of the users who didn't read beyond one page
+# It works under the assumption that if you only read one page, then 
+# you wouldn't have another timestamp from a different page, 
+# Therefore the first and last timestamps would be identical
+timeFilter = userTime['logout time'] != userTime['login time']
+filteredUserTime = userTime[timeFilter]
+
+# Calculates the time difference between timestamps
+filteredUserTime["TotalActiveTime"] = userTime['logout time'] - userTime['login time']
+
+# If I had left in the users who only looked at one page,
+# These results would have been skewed
+print(organiseDescribe(filteredUserTime['TotalActiveTime'].describe()))
+filteredUserTime.to_csv("TotalTimePerUser.csv")
