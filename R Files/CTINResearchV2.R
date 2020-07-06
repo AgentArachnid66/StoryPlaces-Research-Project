335147f479f405882c20024b6b734c002352ef08
AgentArachnid66 <50571566+AgentArachnid66@users.noreply.github.com>
2020-07-06 18:21:34 +0100

    Added page numbers to graphs

--- R Files/CTINResearchV2.R
+++ R Files/CTINResearchV2.R
@@ -4,6 +4,7 @@ require(sf)
 require(sp)
 require(rgdal)
 require(tidyverse)
+require(ggplot2)
 
 
 # Google Cloud API Key
@@ -11,9 +12,18 @@ key <- "AIzaSyD5B3reqaxwM3PXkuYnWY_vtwNZmzETgZQ"
 register_google(key = key )
 # Retrieves a copy of the google map using the mean latitude and longitude as 
 # the centre of focus. 
-ShelleysHeart_basemap <- get_map(location=c(-1.8750486246575349,50.72039837397259), zoom=17, maptype = 'roadmap', source = 'google')
+ShelleysHeart_basemap <- get_map(location=c(-1.8750486246575349,50.72039837397259), zoom=18, maptype = 'roadmap', source = 'google')
 
 p <- ggmap(ShelleysHeart_basemap)
+
+contourData <- st_read("C:/Users/brown/Downloads/contours.kml")
+png(file="contours.png")
+contourMap <- contourData[3]
+ggsave(
+  "contours.png",
+  plot(contourMap)
+)
+
 # Reads the dataframe made in Python into a dataframe that R can use
 rawpageData <- read.csv("C:/Users/brown/.spyder-py3/PageData.csv")
 # Omits the rows without any data in any columns
@@ -46,7 +56,7 @@ p + geom_point(data=pageData, aes(x=Longitude, y=Latitude) ,size=1)
 
 # Filters out the unimportant data from the dataset
 Filtered <- pageData %>% 
-  select(Tag, OriginalIndex,Latitude, Longitude)
+  select(X, Tag, OriginalIndex,Latitude, Longitude)
 # Filters the dataset by the major story branch
 byron <- Filtered %>% 
   filter(Tag == "Byron")
@@ -60,41 +70,64 @@ unTagged <- rawpageData %>%
   select(Tag, OriginalIndex,Latitude, Longitude) %>% 
   filter(Tag=="No Story")
 # So I can easily change the size of the points
-pointSize <- 2
+pointSize <- 10
+
 
 # Plots the Byron Map
-byron_BaseMap <- get_map(location=c(mean(byron$Longitude),mean(byron$Latitude)), zoom=18, maptype = 'roadmap', source = 'google')
+#byron_BaseMap <- get_map(location=c(mean(byron$Longitude),mean(byron$Latitude)), zoom=18, maptype = 'roadmap', source = 'google')
 byron_ggmap <- ggmap(byron_BaseMap)
 byronPoints <- geom_point(data=byron, aes(x=Longitude, y=Latitude), colour = "blue", size=pointSize)
-byronMap <- byron_ggmap + byronPoints
-plot(byronMap)
+byronMap <- byron_ggmap + byronPoints + geom_text(data=byron, aes(x=Longitude, y=Latitude, label=byron$X, colour="white"))
+png(file = "byronMap.png", res =50)
+ggsave(
+  "byronMap.png",
+  plot(byronMap),
+  dpi = 1200
+)
 
 # Plots the Percy Map
-percy_BaseMap <- get_map(location=c(mean(percy$Longitude),mean(percy$Latitude)), zoom=18, maptype = 'roadmap', source = 'google')
+#percy_BaseMap <- get_map(location=c(mean(percy$Longitude),mean(percy$Latitude)), zoom=18, maptype = 'roadmap', source = 'google')
 percy_ggmap <- ggmap(percy_BaseMap)
 percyPoints <- geom_point(data=percy, aes(x=Longitude, y=Latitude), colour="red", size=pointSize)
-percyMap <- percy_ggmap + percyPoints
-plot(percyMap)
+percyMap <- percy_ggmap + percyPoints + geom_text(data=percy, aes(x=Longitude, y=Latitude, label=percy$X, colour="white"))
+png(file = "percyMap.png", res =50)
+ggsave(
+  "percyMap.png",
+  plot(percyMap),
+  dpi = 1200
+)
 
 # Plots the Mary Map
-mary_BaseMap <- get_map(location=c(mean(mary$Longitude),mean(mary$Latitude)), zoom=18, maptype = 'roadmap', source = 'google')
+#mary_BaseMap <- get_map(location=c(mean(mary$Longitude),mean(mary$Latitude)), zoom=18, maptype = 'roadmap', source = 'google')
 mary_ggmap <- ggmap(mary_BaseMap)
 maryPoints <- geom_point(data=mary, aes(x=Longitude, y=Latitude),colour = "orange" ,size=pointSize)
-maryMap <- mary_ggmap + maryPoints
-plot(maryMap)
+maryMap <- mary_ggmap + maryPoints + geom_text(data=mary, aes(x=Longitude, y=Latitude, label=mary$X, colour="white"))
+png(file = "maryMap.png", res =50)
+ggsave(
+  "maryMap.png",
+  plot(maryMap),
+  dpi = 1200
+)
 
 # Plots the John Map
-john_BaseMap <- get_map(location=c(mean(john$Longitude),mean(john$Latitude)), zoom=18, maptype = 'roadmap', source = 'google')
+#john_BaseMap <- get_map(location=c(mean(john$Longitude),mean(john$Latitude)), zoom=18, maptype = 'roadmap', source = 'google')
 john_ggmap <- ggmap(john_BaseMap)
-johnPoints <- geom_point(data=john, aes(x=Longitude, y=Latitude) ,size=pointSize)
-johnMap <- john_ggmap + johnPoints
-plot(johnMap)
+johnPoints <- geom_point(data=john, aes(x=Longitude, y=Latitude),colour="green" ,size=pointSize)
+johnMap <- john_ggmap + johnPoints + geom_text(data=john, aes(x=Longitude, y=Latitude, label=john$X))
+png(file = "johnMap.png", res =50)
+ggsave(
+  "johnMap.png",
+  plot(johnMap),
+  dpi = 1200
+)
 
+p <- geom_density2d()
 # Compiles all of the points and map into one object
 overall <- p + byronPoints
 overall <- overall + percyPoints
 overall <- overall + maryPoints
 overall <- overall+ johnPoints
+overall <- overall + geom_text(data=pageData, aes(x=Longitude, y=Latitude, label=pageData$X))
 
 # Exports the plot as a png
 png(file = "pagePoints.png", res=50)
@@ -104,5 +137,6 @@ ggsave(
   plot(overall),
   dpi = 1200
 )
+
 
 
